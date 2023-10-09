7cfdccc82050fb94998fca0a2ca87a9e0cd4fe9f
Jorge Jara <jorgejarainostroza@gmail.com>
2023-10-09 14:01:04 -0300

    Base pantallas login y registro

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,58 +1,89 @@
 import 'package:flutter/material.dart';
+
 import 'package:miappfeita/pages/register.dart';
 import 'package:miappfeita/pages/todos.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: const Text('Iniciar sesión'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: const Text("Acceder"),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return TodosPage();
-              }));
-            },
-          ),
-          ElevatedButton(
-            child: const Text("Registrar Usuario"),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return RegisterPage();
-              }));
-            },
-          ),
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre de usuario',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nombre de usuario requerido';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contraseña',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Contraseña requerida';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(
+              child: const Text("Acceder"),
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) {
+                  return;
+                }
+
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return TodosPage();
+                }));
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: TextButton(
+                child: const Text("Registrar usuario"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const RegisterPage();
+                  }));
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class CustomScreen extends StatelessWidget {
-  // const CustomScreen({super.key});
-
-  final Color color;
-
-  const CustomScreen({super.key, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      child: Column(children: [
-        ElevatedButton(
-            child: const Text("To Do List"),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return TodosPage();
-              }));
-            })
-      ]),
     );
   }
 }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+import 'package:miappfeita/pages/todos.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _loading = false;
+
+  Future<void> _register() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      _loading = true;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Cuenta creada exitosamente'),
+      ),
+    );
+
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Register'),
+        title: const Text('Registrar usuario'),
       ),
-      body: Center(
-        child: Text('Venata register'),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nombre requerido';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre de usuario',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nombre de usuario requerido';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contraseña',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Contraseña requerida';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirmar contraseña',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Contraseña requerida';
+                  }
+
+                  if (value != _passwordController.value.text.toString()) {
+                    return 'Las contraseñas no coinciden';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _loading ? null : _register,
+              child: _loading
+                  ? const Text("Registrando...")
+                  : const Text("Registrarse"),
+            ),
+          ],
+        ),
       ),
     );
   }
