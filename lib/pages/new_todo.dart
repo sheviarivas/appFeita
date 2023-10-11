3cfddd742c11a13a43709bb2b888d501a32af085
shevia2017 <shevia2017@inf.udec.cl>
2023-10-11 19:06:38 -0300

    Frontend de ventanas new_todo y todos

--- lib/pages/new_todo.dart
+++ lib/pages/new_todo.dart
@@ -1,14 +1,95 @@
 import 'package:flutter/material.dart';
 
-class NewTodoPage extends StatelessWidget {
+// import 'package:miappfeita/pages/register.dart';
+// import 'package:miappfeita/pages/todos.dart';
+// import 'package:miappfeita/utils/auth.dart';
+
+class NewTodoPage extends StatefulWidget {
+  const NewTodoPage({super.key});
+
+  @override
+  State<NewTodoPage> createState() => _NewTodoPageState();
+}
+
+class _NewTodoPageState extends State<NewTodoPage> {
+  // final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _taskTitleController = TextEditingController();
+  final TextEditingController _taskDescriptionController =
+      TextEditingController();
+
+  bool _loading = false;
+
+  Future<void> _saveTask(BuildContext context) async {
+    // if (!_formKey.currentState!.validate()) {
+    //   return;
+    // }
+
+    setState(() {
+      _loading = true;
+    });
+
+    final taskTitle = _taskTitleController.value.text.toString();
+    final taskDescription = _taskDescriptionController.value.text.toString();
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("New To do"),
+        title: const Text('New TODO'),
       ),
-      body: Center(
-        child: Text('cositas pa un nuevo to do'),
+      body: Form(
+        // key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: _taskTitleController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Título de la nueva tarea',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Título requerido';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: _taskDescriptionController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Descripción',
+                ),
+                // validator: (value) {
+                //   if (value == null || value.isEmpty) {
+                //     return 'Contraseña requerida';
+                //   }
+
+                //   return null;
+                // },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _loading ? null : () => _saveTask(context),
+              child:
+                  _loading ? const Text("Guardando...") : const Text("Guardar"),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/todos.dart
+++ lib/pages/todos.dart
@@ -4,52 +4,138 @@ import 'package:miappfeita/shared/barra_lateral.dart';
 class TodosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const items = 10;
     return Scaffold(
-      drawer: const Drawer(
-        child: BarraLateral(),
-      ),
-      appBar: AppBar(
-        title: const Text("Mi app feita TODO LISTaaaa"),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Text('Tasks'),
+        drawer: const Drawer(
+          child: BarraLateral(),
         ),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Mi app feita TODO LIST"),
+        ),
+        body: Container(
+            // padding: const EdgeInsets.all(18),
+            // child: ListView(children: [
+            child: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: List.generate(
+                  items,
+                  (index) => ItemWidget(
+                        name: 'Item $index',
+                        description:
+                            'ksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksd hakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjshksdhakjsh',
+                      )),
+            ),
+          );
+        })));
   }
 }
 
-class Task extends StatelessWidget {
-  // const Task({required this.color});
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.name,
+    required this.description,
+  });
+
+  final String name;
+  final String description;
+
+  Future<void> _deleteTask(BuildContext context) async {
+    return;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Text(
-              "Nombre1",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
+    return Container(
+      padding: EdgeInsets.only(bottom: 15),
+      child: Column(
+        children: [
+          Row(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Card(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text("MAR 15"),
+                ),
+              ),
+              Card(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text("MAR 17"),
+                ),
+              ),
+            ],
+          ),
+          Card(
+              child: SizedBox(
+            height: 100,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          textAlign: TextAlign.left,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          description,
+                          textAlign: TextAlign.left,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w400),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 6),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RawMaterialButton(
+                            shape: const CircleBorder(),
+                            onPressed: () => _deleteTask(context),
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.redAccent,
+                              size: 40,
+                            )),
+                        Row(
+                          children: [
+                            const Icon(Icons.circle,
+                                color: Colors.orange, size: 20),
+                            const Icon(Icons.circle,
+                                color: Colors.green, size: 20),
+                            // const Icon(Icons.circle,
+                            //     color: Colors.blue, size: 20),
+                            const Text("-"),
+                            const Icon(Icons.circle,
+                                color: Colors.grey, size: 20),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-            Text(
-              "Descripcion1",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        )
-      ],
+          ))
+        ],
+      ),
     );
   }
 }
